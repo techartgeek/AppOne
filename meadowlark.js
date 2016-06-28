c67484b26678b1c68c0a55e08dd60efee46931aa
techartgeek <vikasharma150880@gmail.com>
2016-06-28 17:35:30 +0530

    c1

--- meadowlark.js
+++ meadowlark.js
@@ -2,8 +2,18 @@ var express = require('express');
 
 var app = express();
 
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple."
+];
+
 //set up handlebars view engine
-var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
+var handlebars = require('express3-handlebars').create({
+    defaultLayout: 'main'
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -14,28 +24,28 @@ app.use(express.static(__dirname + '/public'));
 app.set('port', process.env.PORT || 3000);
 
 //home page
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
     res.render('home');
 });
 
 //about page
-app.get('/about', function(req, res){
+app.get('/about', function (req, res) {
     res.render('about');
 });
 
 //custom 404 catch-all handler (middleware)
-app.use(function(req, res, next){
+app.use(function (req, res, next) {
     res.status(404);
     res.render('404');
 });
 
 //custom 500 error handler page (middleware)
-app.use(function(err, req, res, next){
+app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500);
     res.render('500');
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate...');
 });
