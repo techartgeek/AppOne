1d7bb9e3c271bf018f750cd6e92fd99aa7aa17cb
techartgeek <vikasharma150880@gmail.com>
2016-06-28 17:06:41 +0530

    c1

--- meadowlark.js
+++ meadowlark.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 
 //set up handlebars view engine
-var handlebars = require('express3-handlebars');
-handlebars.create({defaultLayout: 'main'});
+var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
